bd8fbb96d2cb9faf025d72d56328dd88cee2e49c
SauravJalui <sauravjalui.007@gmail.com>
2020-08-20 09:38:49 +0530

    Added tests for Anagram check

---
+++ Anagram/tests.py
@@ -0,0 +1,19 @@
+import unittest
+from AnagramCheck import Solution
+
+obj = Solution()
+
+
+class AnagramTests(unittest.TestCase):
+
+    def test_1(self):
+        """anagram('dog','god') returns True"""
+        self.assertEqual(obj.anagram('dog','god'), True)
+
+    def test_2(self):
+        """anagram(''clint eastwood','old west action') returns True"""
+        self.assertEqual(obj.anagram('clint eastwood','old west action'), True)
+
+
+if __name__ == '__main__':
+    unittest.main()
